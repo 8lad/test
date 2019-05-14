2088047fda3b2aadfdf6cff7512ed3dd221fc958
Vlad <example@mail.com>
2019-05-14 23:15:47 +0300

    second commit

--- js.js
+++ js.js
@@ -12,4 +12,4 @@ var meditateResult1 = prompt("Во сколько обойдется?", "");
 var everedayCash2 = prompt("Введите обязательную статью расходов в этом месяце", "");
 var meditateResult2 = prompt("Во сколько обойдется?", "");
 var expenses = { firstAnswerCash1: everedayCash1, secondAnswerResult1: meditateResult1, firstAnswerCash2: everedayCash2, secondAnswerResult2: meditateResult2 };
-alert(appData[moneyData] / 30 + "Бюджет на один день");
+alert(appData.moneyData / 30 + " Бюджет на один день");
