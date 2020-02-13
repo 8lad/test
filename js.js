6cf9134b8afa6ac29182e446e5b369937f4b8477
8lad <vladisla1988@gmail.com>
2020-02-13 23:19:06 +0200

    added new cycle

--- js.js
+++ js.js
@@ -1,15 +1,61 @@
-var money = prompt("Ваш бюджет на месяц?", "");
-var time = prompt("Введите дату в формате YYYY-MM-DD", "");
-var appData = {
-    timeData: time,
-    moneyData: money,
-    optionalExpenses: null,
-    income: null,
-    savings: false
+let money = +prompt("Ваш бюджет на месяц?", ""),
+    time = prompt("Введите дату в формате YYYY-MM-DD", ""),
+    appData = {
+        timeData: time,
+        budget: money,
+        optionalExpenses: {},
+        income: [],
+        savings: false,
+        expenses: {}
+    };
+
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        b = prompt("Во сколько обойдется?", "");
+    if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+        console.log('done');
+        appData.expenses[a] = b;
+    } else {
+        alert('Please reload the page and input numbers');
+        i--;
+    }
 };
-var everedayCash1 = prompt("Введите обязательную статью расходов в этом месяце", "");
-var meditateResult1 = prompt("Во сколько обойдется?", "");
-var everedayCash2 = prompt("Введите обязательную статью расходов в этом месяце", "");
-var meditateResult2 = prompt("Во сколько обойдется?", "");
-var expenses = { firstAnswerCash1: everedayCash1, secondAnswerResult1: meditateResult1, firstAnswerCash2: everedayCash2, secondAnswerResult2: meditateResult2 };
-alert(appData.moneyData / 30 + " Бюджет на один день");
+// let i = 0;
+// while (i < 2) {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//         b = prompt("Во сколько обойдется?", "");
+//     if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+//         console.log('done');
+//         appData.expenses[a] = b;
+//     } else {
+//         alert('Please reload the page and input numbers');
+//         i--;
+//     }
+//     i++;
+// };
+// let i = 0;
+// do {
+//     let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//         b = prompt("Во сколько обойдется?", "");
+//     if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+//         console.log('done');
+//         appData.expenses[a] = b;
+//     } else {
+//         alert('Please reload the page and input numbers');
+//         i--;
+//     }
+//     i++;
+// } while (i < 2);
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Ежедневный бюджет:" + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log("Not enough");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Middle level");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("High level");
+} else {
+    console.log("Error");
+}
